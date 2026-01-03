b2b2b79e5cf2f3e71135167a62984dc139ba36ec
raditha dissanayake <raditha@raditha.com>
2026-01-03 21:24:37 +0530

    A really fake calculator

---
+++ src/main/java/com/raditha/bertie/testbed/simple/Calculator.java
@@ -0,0 +1,15 @@
+package com.raditha.bertie.testbed.simple;
+
+public class Calculator {
+    public int addAndDouble1(int a, int b) {
+        int sum = a + b;
+        int result = sum * 2;
+        return result;
+    }
+
+    public int addAndDouble2(int x, int y) {
+        int sum = x + y;
+        int result = sum * 2;
+        return result;
+    }
+}
