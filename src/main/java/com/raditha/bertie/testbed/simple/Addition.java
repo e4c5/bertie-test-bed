de8adae57759690f2a88a8b09a8d048067b96007
raditha dissanayake <raditha@raditha.com>
2026-01-03 19:02:08 +0530

    A couple of test classes were moved out of bertie main project

---
+++ src/main/java/com/raditha/bertie/testbed/simple/Addition.java
@@ -0,0 +1,19 @@
+package com.raditha.bertie.testbed.simple;
+
+public class Addition {
+    void method1() {
+        int x = 1;
+        int y = 2;
+        System.out.println(x + y);
+        System.out.println("result");
+        System.out.println("done");
+    }
+
+    void method2() {
+        String a = "hello";
+        String b = "world";
+        System.out.println(a + b);
+        System.out.println("output");
+        System.out.println("finished");
+    }
+}
---
+++ src/main/java/com/raditha/bertie/testbed/simple/Printing.java
@@ -0,0 +1,21 @@
+package com.raditha.bertie.testbed.simple;
+
+public class Printing {
+    void method1() {
+        String name = "John";
+        String email = "john@test.com";
+        int age = 25;
+        System.out.println("Name: " + name);
+        System.out.println("Email: " + email);
+        System.out.println("Age: " + age);
+    }
+
+    void method2() {
+        String name = "Jane";
+        String email = "jane@test.com";
+        int age = 30;
+        System.out.println("Name: " + name);
+        System.out.println("Email: " + email);
+        System.out.println("Age: " + age);
+    }
+}
