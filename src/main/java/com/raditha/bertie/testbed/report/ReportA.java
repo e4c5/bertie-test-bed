6d5f8a05819cdf076b6787f20b3fd946f9c2e1d5
raditha dissanayake <raditha@raditha.com>
2026-01-22 06:27:48 +0530

    Restore ReportA and ReportB for refactoring verification

---
+++ src/main/java/com/raditha/bertie/testbed/report/ReportA.java
@@ -0,0 +1,10 @@
+package com.raditha.bertie.testbed.report;
+
+public class ReportA {
+    public void generate() {
+        System.out.println("Initializing Report A");
+        int count = 100;
+        System.out.println("Processing " + count + " items");
+        System.out.println("Report A completed");
+    }
+}
---
+++ src/main/java/com/raditha/bertie/testbed/report/ReportB.java
@@ -0,0 +1,10 @@
+package com.raditha.bertie.testbed.report;
+
+public class ReportB {
+    public void generate() {
+        System.out.println("Initializing Report B");
+        int count = 200;
+        System.out.println("Processing " + count + " items");
+        System.out.println("Report B completed");
+    }
+}
